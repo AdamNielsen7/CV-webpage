1c76d845d05c84a80f6a55c73e8ec1d87a430216
adamnielsen7 <adam97@byui.edu>
2021-07-21 14:58:59 -0600

    added anime library call

--- js/index.js
+++ js/index.js
@@ -1,12 +1,48 @@
-//Collabsible Categories Event Listener
-document.querySelectorAll(".collapseButton").forEach(button => {
-    button.addEventListener('touchend', e=> {
-        let content = e.target.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+document.querySelectorAll("button.contentTitle").forEach(button => {
+    button.addEventListener('click', function() {
+        this.classList.toggle("expanded");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display ="none";
         }
+        else {
+            content.style.display = "block";
+        }
+    })
+})
+
+function displayAnime(show) {
+    const list = document.getElementById("animeList");
+    let entry = document.createElement("a");
+    entry.target = "_blank";
+    entry.href = show.link;
+    let image = document.createElement("img");
+    image.src = show.image;
+    image.alt = show.title;
+    entry.appendChild(image);
+    list.appendChild(entry);
+}
+
+var kitsuUserID = 44855
+async function getKitsuLibrary(kitsuUserID) {
+    fetch('https://kitsu.io/api/edge/library-entries?filter[userId]=' + kitsuUserID)
+    .then(response => response.json())
+    .then(library => {
+        library.data.forEach(entry => {
+            fetch(entry.relationships.anime.links.related)
+            .then(response => response.json())
+            .then(anime => {
+                const attributes = anime.data.attributes;
+                const link = "https://kitsu.io/anime/" + attributes.slug;
+                const show = {title: attributes.titles.en,
+                            link: link,
+                            image: attributes.posterImage.small}
+                displayAnime(show);
+            });
+        })
     });
-});
+}
+
+getKitsuLibrary(kitsuUserID)
+
 
